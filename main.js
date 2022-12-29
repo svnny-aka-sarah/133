e2e63dbd4306de8558059ddb2bd2d9404a7c98f3
svnny-aka-sarah <116078227+svnny-aka-sarah@users.noreply.github.com>
2022-12-29 22:29:07 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+status = "";
+img = "";
+objects = [];
+objectDetector = "";
+
+function preLoad()
+{
+img=loadImage('book.jpg');
+}
+
+
+function setup()
+{
+    canvas= createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult); 
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results ;
+    console.log("");
+}
+
+function draw()
+
+{
+if(status != "")
+ 
+    image(img, 0, 0, 680, 420);
+    for (var i=0; i < objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Status: object detected";
+        
+        fill(255, 0, 0);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 5,objects[i].y + 15 );
+        noFill();
+        stroke(255, 0, 0);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    
+    }
+ }
+
+
